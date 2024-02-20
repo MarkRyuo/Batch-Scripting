ebb95b176df3969e4bf90d005e3701706e1d1799
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-20 21:40:10 +0800

    update herEW

--- second.bat
+++ second.bat
@@ -1,7 +1,8 @@
 @echo off 
-title "This is my second in batch scripting "
+title  This is my second in batch scripting 
+echo Hello bro !
 pause 
 cls @REM Para ma clear ang nasa taas kapa inenter na 
 
-title "Enter!" 
+echo "Enter!" 
 pause @REM for ending the lines
