d1ba7c23b9fe0c5d9c202d04144293a19576b53a
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-23 20:56:31 +0800

    update herE

--- 4th.bat
+++ 4th.bat
@@ -2,4 +2,6 @@ echo off
 title This is 4th 
 echo Hello This is Fourth 
 pause 
-cls @REM this is for if nag enter ka mawawala yung nasa taas 
+cls @REM this is for if nag enter ka mawawala yung nasa taas
+echo Please Enter to Quit 
+pause  
