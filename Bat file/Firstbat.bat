900845cee1004c814b7a1fa31ca6c0f99721921b
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-24 21:37:18 +0800

    update herE

--- Bat file/Firstbat.bat
+++ Bat file/Firstbat.bat
@@ -1,8 +1,18 @@
-@echo off 
-title This is my first batch scripting 
-echo Hello World! 
-pause 
-cls 
-echo Enter your Name 
-pause 
+@REM @echo off 
+@REM title This is my first batch scripting 
+@REM echo Hello World! 
+@REM pause 
+@REM cls 
+@REM echo Enter your Name 
+@REM pause 
+
+@REM @echo off 
+@REM title Practical joke 
+@REM echo Are you gay? "Please Enter"
+@REM pause @REM this is for stopping 
+@REM cls @REM para kapag i enter ay mawala yung nasa taas 
+
+@REM echo Yeah you are gay !
+@REM pause 
+
 
