ac1aefb303b15b281008908843e84149e74c1416
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-01 19:13:09 +0800

    update herE

--- 2nd/1st.bat
+++ 2nd/1st.bat
@@ -2,4 +2,4 @@ echo off
 title Batchfile2 
 echo ENTER TO START THE MUSIC!
 pause
-start https://www.youtube.com/watch?v=O4BIP0A7CIw&t=1826s  
+start https://www.youtube.com/watch?v=O4BIP0A7CIw&t=1826s @REM Direct to the youtube!  
