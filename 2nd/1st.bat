08423df41e58ddd256b9e64ea831ab9fee5f8396
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-01 19:18:46 +0800

    udpaet herE

--- 2nd/1st.bat
+++ 2nd/1st.bat
@@ -9,3 +9,21 @@ color 0b
 title colors
 echo This is changing color!
 pause 
+
+
+@REM 0 = Black
+@REM 1 = Blue
+@REM 2 = Green
+@REM 3 = Aqua
+@REM 4 = Red
+@REM 5 = Purple
+@REM 6 = Yellow
+@REM 7 = White
+@REM 8 = Gray
+@REM 9 = Light Blue
+@REM A = Light Green
+@REM B = Light Aqua
+@REM C = Light Red
+@REM D = Light Purple
+@REM E = Light Yellow
+@REM F = Bright Whit
