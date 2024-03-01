b1d53403686ffe822c41731d944914d11345eec4
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-01 19:13:49 +0800

    update herE

--- 2nd/1st.bat
+++ 2nd/1st.bat
@@ -1,5 +1,6 @@
-echo off 
-title Batchfile2 
-echo ENTER TO START THE MUSIC!
-pause
-start https://www.youtube.com/watch?v=O4BIP0A7CIw&t=1826s @REM Direct to the youtube!  
+@REM echo off 
+@REM title Batchfile2 
+@REM echo ENTER TO START THE MUSIC!
+@REM pause
+@REM start https://www.youtube.com/watch?v=O4BIP0A7CIw&t=1826s @REM Direct to the youtube!  
+
