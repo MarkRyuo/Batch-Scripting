f7ad1bde749616e269693ccf5a490816c46eb01d
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-02 21:20:18 +0800

    udpateehrE

--- 2nd/1st.bat
+++ 2nd/1st.bat
@@ -4,11 +4,11 @@
 @REM pause
 @REM start https://www.youtube.com/watch?v=O4BIP0A7CIw&t=1826s @REM Direct to the youtube!  
 
-echo off 
-color 0b
-title colors
-echo This is changing color!
-pause 
+@REM echo off 
+@REM color 0b
+@REM title colors
+@REM echo This is changing color!
+@REM pause 
 
 
 @REM 0 = Black
