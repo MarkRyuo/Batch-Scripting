f07c1ae80311b96fe9bb2e2e36edc378b6abbae4
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-12 20:22:30 +0800

    udpaet ehrE

--- main.bat
+++ main.bat
@@ -1,10 +1,12 @@
-echo off 
-color 4d 
-title Batch 
-echo "Batch Scripting is fun Right?"
-pause 
-echo "Yeah This is Fun"
-pause 
-cls
-echo "Enter if You are done" 
-pause 
+@REM echo off 
+@REM color 4d 
+@REM title Batch 
+@REM echo "Batch Scripting is fun Right?"
+@REM pause 
+@REM echo "Yeah This is Fun"
+@REM pause 
+@REM cls
+@REM echo "Enter if You are done" 
+@REM pause 
+
+
